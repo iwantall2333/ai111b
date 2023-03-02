5c87585edc6660f16a4ef293ee592e9895b54cfc
iwantall2333 <s110910540@student.nqu.edu.tw>
2023-03-02 12:12:33 +0800

    hw2

--- 習題2.py
+++ 習題2.py
@@ -1,17 +1,24 @@
 import random
 import copy
 
-def total(point): # 算總距離
-	sum1 = 0
-	for i in range(len(point)-1):
-		sum1 += (abs(point[i][0]-point[i+1][0])+abs(point[i][1]-point[i+1][1])) ** 0.5
-	return sum1
+def total(point,s): # 算總距離
+    sum1 = 0
 
-def meter(a,b,point): # 暫時算總距離
+    pointTM = point.copy()
+    for i in range (len(point)-1):
+        pointTM[i] = point[s[i]]  # 變成換過順序的point
+
+    for i in range(len(point)-1):
+        sum1 += (abs(pointTM[i][0] -pointTM[i+1][0]) + abs(pointTM[i][1] -pointTM[i+1][1])) ** 0.5
+
+    return sum1
+
+def meter(a,b,point,s): # 暫時算總距離
 
     sum = 0
     pointTM = point.copy()
-    pointTM[a],pointTM[b] = pointTM[b],pointTM[a] 
+    for i in range (len(point)-1):
+        pointTM[i] = point[s[i]]  # 變成換過順序的point
 
     for i in range(len(point)-1):
         sum += (abs(pointTM[i][0] -pointTM[i+1][0]) + abs(pointTM[i][1] -pointTM[i+1][1])) ** 0.5
@@ -35,10 +42,10 @@ def start():
 
     # code
     for i in range(100000):
-        if(meter(a,b,point)<temp):
+        if(meter(a,b,point,s)<temp):
             s[rd1],s[rd2]=s[rd2],s[rd1]
 
-    print("length: ",total(point)," set: ",s)
+    print("length: ",total(point,s)," set: ",s)
    
 
 start()
@@ -46,15 +53,15 @@ start()
 '''
 執行結果
 ai111b>python 習題2.py
-[6, 5, 9, 2, 0, 1, 3, 4, 7, 8]
-length:  11.23606797749979  set:  [6, 5, 9, 2, 0, 1, 3, 4, 7, 8]
+[8, 0, 4, 3, 1, 9, 2, 7, 6, 5]
+length:  16.26868186481444  set:  [8, 0, 4, 3, 1, 9, 2, 7, 6, 5]
 
 ai111b>python 習題2.py
-[3, 8, 4, 9, 0, 7, 1, 2, 6, 5]
-length:  11.23606797749979  set:  [3, 8, 4, 9, 0, 7, 1, 2, 6, 5]
+[5, 6, 0, 2, 7, 9, 4, 3, 1, 8]
+length:  14.861041012060838  set:  [5, 6, 0, 2, 7, 9, 4, 3, 1, 8]
 
 ai111b>python 習題2.py
-[6, 9, 8, 1, 2, 7, 5, 3, 4, 0]
-length:  11.23606797749979  set:  [6, 9, 8, 1, 2, 7, 5, 3, 4, 0]
+[7, 8, 6, 5, 0, 9, 1, 4, 2, 3]
+length:  15.246035652598035  set:  [7, 8, 6, 5, 0, 9, 1, 4, 2, 3]
 '''
 
